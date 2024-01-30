38e8c00f365dd6158d8b6682d0f063199c1031f6
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-30 20:46:52 +0100

    first commit

--- ML-ProgrammingCourse/README.md
+++
@@ -1,2 +0,0 @@
-# ML-ProgrammingCourse
-Curso básico de programación para MyLearny
--- ML-ProgrammingCourse/index.js
+++
@@ -1,4 +0,0 @@
-let currentYear = document.getElementById('currentYear')
-year =  new Date().getFullYear();
-
-currentYear.innerHTML = year
