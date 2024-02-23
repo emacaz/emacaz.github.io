5805704a6db9c6c8b4042c0d7ebe45c3b0b8c9ed
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-02-23 11:12:01 +0100

    Starting projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,51 @@
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus, deleniti aliquam suscipit velit voluptatem repellendus
+                architecto id vero alias similique, dolorum delectus. Vero
+                dolore magnam sapiente officia maxime voluptatem! Nostrum?
+              </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
