a5495aaf95ff0288a48c6d764acb9b44b2100166
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-02-26 12:17:53 +0100

    Working on Contact and Project sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,11 @@
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import TrackVisibility from "react-on-screen";
+
 export const Projects = () => {
   const projects = [
     {
